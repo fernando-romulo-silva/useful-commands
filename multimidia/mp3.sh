306ce1228d8fbad7853dc08882cf654c08370f4b
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2019-04-17 09:18:57 -0300

    news

--- multimidia/mp3.sh
+++ multimidia/mp3.sh
@@ -35,3 +35,15 @@ edit file and put 'Silecen.mp3' between the files
 
 # concat audio files
 ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mp3
+
+
+
+# concat mp4 files
+
+:: Create File List
+echo file file1.mp4 >  mylist.txt 
+echo file file2.mp4 >> mylist.txt
+echo file file3.mp4 >> mylist.txt
+
+:: Concatenate Files
+ffmpeg -f concat -i mylist.txt -c copy output.mp4
