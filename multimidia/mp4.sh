2161c33980e44dfebf774bc721a03a5bf642012c
Fernando <fernando.romulo.silva@gmail.com>
2020-12-16 16:02:18 -0300

    news

--- multimidia/mp4.sh
+++ multimidia/mp4.sh
@@ -2,9 +2,16 @@
 #### Concat mp4 files #########################################################################################################################
 
 # Create File List
-echo file file1.mp4 >  mylist.txt 
-echo file file2.mp4 >> mylist.txt
-echo file file3.mp4 >> mylist.txt
+for f in *.mp4; do echo "file '$f'" >> mylist.txt; done
 
 # Concatenate Files
-ffmpeg -f concat -i mylist.txt -c copy output.mp4
+ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mp4
+
+
+
+# If they are NOT exactly same (100% same codec, same resolution, same type) MP4 files
+ffmpeg -i myfile1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts temp1.ts
+ffmpeg -i myfile2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts temp2.ts
+
+# now join
+ffmpeg -i "concat:temp1.ts|temp2.ts" -c copy -bsf:a aac_adtstoasc output.mp4
