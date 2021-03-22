9825abdc7c5918285ad1eb6f76339ec6de2dc156
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-03-22 08:33:17 -0300

    news

--- dockers-and-cia/docker.sh
+++ dockers-and-cia/docker.sh
@@ -11,11 +11,21 @@ docker volume prune
 # Create a image (from Dockerfile)
 docker build -t image_name .
 
-# Run the image
-docker run -d -p 80:80 --name container_name image_name
+# Create a image (from Dockerfile)
+docker image build -f src/main/docker/Dockerfile.something -t prefix/image_name .
+
+# List prefix's images
+docker image ls | grep prefix/
 
 # List images
 docker images
 
 # Remove an image
 docker rmi 59ada6b2b508
+
+
+# Run the image
+docker run -d -p 80:80 --name container_name image_name
+
+
+docker run -i -p 80:80 --rm --name container_name image_name
