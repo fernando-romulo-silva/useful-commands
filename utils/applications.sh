68edbcc5d2276e43749a0099e3edef315937d079
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-03-30 07:33:52 -0300

    news

--- utils/applications.sh
+++ utils/applications.sh
@@ -2,15 +2,18 @@
 
 find . -type f -name "*.odt" | while read i ; do libreoffice --headless --convert-to pdf "${i}"; done
 
+
 #### Change "_" for " " files name ############################################################################################
 
 for file in * ; do mv -v "$file" "$(echo $file | sed 's/_/ /g')" ; done
+
 
 #### Convert image to PDF #####################################################################################################
 
 convert *.png  -compress jpeg -resize 1240x1753  -extent 1240x1753 -gravity center -units PixelsPerInch -density 150x150 multipage.pdf
 
 convert *.png  -compress jpeg -gravity center   -units PixelsPerInch -density 300x300 multipage.pdf
+
 
 #### Convert txt to PDF #####################################################################################################
 
@@ -20,8 +23,9 @@ for file in *.txt; do libreoffice --convert-to "pdf" "$file"; done
 
 for file in *.txt; do pandoc --latex-engine=xelatex "$file" -o "${file%txt}"pdf; done
 
+
 #### Replace text on txt files ##############################################################################################
 
 sed -i '/texto/' *txt # remove 'texto' string from txt files
 
-sed -i 's/,/;/g' *txt # repalce ',' to ';' on txt files
+sed -i 's/,/;/g' *txt # replace ',' to ';' on txt files
---
+++ utils/terminal.sh
@@ -0,0 +1,143 @@
+#### Check Disk ###################################################################################################
+
+# inodes
+df -i
+
+# disk
+df -h
+
+
+#### Check open ports ##############################################################################################
+
+netstat -atunp
+
+
+#### Terminal's History ###########################################################################################
+
+# last 10 elements history
+history | tail
+
+# find element on history
+history | grep df
+
+# 165  for file in *.txt; do libreoffice --convert-to "pdf" "$file"; done
+# 233  history | grep pdf
+# 234  for file in *.txt; do libreoffice --convert-to "pdf" "$file"; done
+# 382  df -h
+
+# execute the command 382: df -h
+!382
+
+# show the command 382:
+!382:p
+
+df -h
+
+# reverse search
+Ctrl + r
+
+# execute the last command selected
+!df
+
+# show the last command executed
+!!
+
+# pass parameter to command
+touch arq1
+
+# first
+ls -lha !^
+# result touch arq1
+
+# last 
+ls -lha !$
+
+# all
+ls -lha !*
+
+# param pos 2 last command's cp
+ls -lha !cp:2
+
+# all param last command's cp
+ls -lha !cp:*
+
+# remove from history
+history -d 452
+
+#### Check disk size ##############################################################################################
+
+du -sh
+
+
+#### Who am  I ####################################################################################################
+
+whoami
+
+
+#### Read Server Logs #############################################################################################
+
+tail -f server/default/log/server.log | grep "something"
+
+tail -n 1000 server/default/log/server.log
+
+less server/default/log/server.log
+
+cat /var/log/docker/containers/sonny/server.log | grep alguma coisa
+
+
+
+#### Starts Java Servers ##########################################################################################
+
+nohup ./run.sh -DappEnv=dev -c default -b 0.0.0.0 & 
+
+nohup ./standalone.sh -DappEnv=dev -b 0.0.0.0 & 
+
+
+#### Kill process #################################################################################################
+
+ps aux | grep jboss  # (find proccess â€œpidâ€� )
+
+ps -ef | grep firefox
+
+# Or
+
+jps (pids de processos java)
+
+kill -9 "pid" "process' identification"
+
+# Or
+
+fuser -k 8080/tcp
+
+
+#### Memory available #############################################################################################
+
+free -m -t
+
+
+#### Processor and Memory #########################################################################################
+
+top # press q
+
+# ou
+
+htop # press q
+
+#### Check the port is open #######################################################################################
+
+lsof -i tcp:5005
+
+
+#### Add executable ###############################################################################################
+
+chmod +x
+
+
+#### Clean Debian's based distro ##################################################################################
+
+sudo apt-get autoclean
+
+sudo apt-get autoremove
+
+sudo deborphan --guess-data | xargs sudo apt-get -y remove --purge
+
