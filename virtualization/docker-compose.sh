0ab1c9251b282a3ae9f9e038397439270c289480
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-05-03 21:26:37 -0300

    news

--- virtualization/docker-compose.sh
+++ virtualization/docker-compose.sh
@@ -7,8 +7,15 @@
 # * configs
 
 
-# Starts all containers defined in a Docker Compose file
-docker-compose -f docker-compose.yaml up -d
+# Starts all containers defined in a Docker Compose file -d is background
+docker-compose up -d
+
+# or
+$ docker-compose -f prod-equus-bass.yml up -d
+
 
 # Down Stops all containers defined in a Docker Compose file
-docker-compose -f docker-compose.yaml 
+docker-compose down
+
+# or
+docker-compose -f docker-compose.yaml down
