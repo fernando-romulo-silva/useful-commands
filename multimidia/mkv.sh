4c62fd11b10d6d7b6bb69fa125f4ed64b82a2506
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2024-06-07 12:06:05 -0300

    news

--- multimidia/mkv.sh
+++ multimidia/mkv.sh
@@ -9,3 +9,7 @@ $ ffmpeg -i input.mkv -ss 00:02:31  -c:v libx264 -map 0:v:0 -map 0:a:0 -map 0:s 
 $ for f in *.mkv; do mv "$f" "${f%.mkv}-old.mkv"; ffmpeg -i "${f%.mkv}-old.mkv" -c:v libx264 -map 0:v:0 -map 0:a:0 -map 0:s -c:a libvo_aacenc "${f}"mkv -hide_banner;  done
 
 $ for f in *.mkv; do mv "$f" "${f%.mkv}-old.mkv"; ffmpeg -i "${f%.mkv}-old.mkv" -c:v libx264 -map 0:v:0 -map 0:a:0 -map 0:s -c:a libvo_aacenc "${f}" -hide_banner; rm "${f%.mkv}-old.mkv"; done
+
+#### Remove first audio track from mkv file
+
+$ ffmpeg -i input.mkv -map '0:v' -map '0:2' -c:v copy -c:a copyoutput-cut.mkv
---
+++ virtualization/docker/tools.sh
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------------------------------------
+# -------- Docker dive
+# To analyze a Docker image simply run dive with an image tag/id/digest:
+#
+$ dive <your-image-tag>
+
+# --------------------------------------------------------------------------------------------------------
+# -------- Hadolint
+# A smarter Dockerfile linter that helps you build best practice Docker images. 
+# 
+$ hadolint <Dockerfile>
