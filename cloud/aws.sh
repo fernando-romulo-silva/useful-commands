b0e27f32dbe42474e8b58c8056b3995ef977eff8
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2024-07-12 08:32:47 -0300

    news news

---
+++ cloud/aws.sh
@@ -0,0 +1,17 @@
+# init
+
+# configure credentials
+$ aws configure
+AWS access Key ID [None]: [some key]
+AWS secret Access Key [None]: [some access key]
+Default region nae [None]: sa-east-1
+Default output format [None]: json
+
+# list specific objects
+$ aws s3 ls
+
+# create a bucket
+$ aws s3 mb s3://mybuket-2eda3s
+
+# list users
+$ aws list-users
