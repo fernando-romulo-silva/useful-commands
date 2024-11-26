77e561aecffe5bae8d32374564869f106450555b
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2024-11-26 08:44:22 -0300

    news

--- internet/youtube.sh
+++ internet/youtube.sh
@@ -1,13 +1,15 @@
-# https://github.com/rg3/youtube-dl
+# https://github.com/yt-dlp/yt-dlp
 
 # donwload youtube video with subtitle eng
-$ youtube-dl --write-sub --skip-download --sub-lang=en "https://www.youtube.com/watch?v=YOUR VIDEO HERE"
+$ yt-dlp https://www.youtube.com/watch?v=5fk2QNkFAnE -f 'bv*[height=720]+ba' --write-auto-subs --sub-lang "en.*"
 
 # download youtube subtitle
-$ youtube-dl --convert-subs srt --sub-lang=en --write-auto-sub --skip-download https://youtu.be/UZ9VKxxGo58
+$ youtube-dl --convert-subs srt --sub-lang=en --write-auto-sub --skip-download 
 
 # download youtube with 
 $ youtube-dl "bestvideo[height<=720][ext=mp4]+bestaudio/[height <=? 720]" -cio '%(autonumber)s - %(title)s.%(ext)s' --restrict-filenames https://www.youtube.com/watch?v=YOUR VIDEO HERE --sub-lang en --convert-subs srt --write-sub
 
 # download a youtube playlist
 $ youtube-dl -f "bestvideo[height<=480][ext=mp4]+bestaudio/[height <=? 720]" --playlist-reverse -cio '%(autonumber)s - %(title)s.%(ext)s' --restrict-filenames https://www.youtube.com/playlist?list=YOUR LIST HERE --sub-lang en --convert-subs srt --write-sub
+
+
--- multimidia/subtitle.sh
+++ multimidia/subtitle.sh
@@ -8,3 +8,10 @@ $ ffmpeg -i input.mp4 -f srt -i input.srt -map 0:0 -map 0:1 -map 1:0 -c:v copy -
 
 # Mux mkv
 $ ffmpeg -i input.mkv -i input.srt -map 0 -map 1 -c copy output.mkv
+
+# In case of the error invalid UTF-8 in decoded subtitles text;
+## first check file encode: file -bi input.srt
+$ file -bi movie.srt
+
+## Pass the encode to ffmepg:
+$ ffmpeg -i infile.mp4 -sub_charenc utf-8 -i infile.srt -c copy -c:s mov_text outfile.mp4
